f66289406bb6b0e8dc39c89ad84e025469ce4572
SLavrianov <stefan.lavrianov.ext@diagroup.com>
2023-10-27 16:52:38 +0200

    corregir conexion database

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ import {createPool} from "mysql2/promise";
 import config from "./../config.js";
 
 export const getConnection = createPool({
-    host: config.host,
+    host: config.dbhost,
     database: config.database,
     user: config.user,
     password: config.password,
